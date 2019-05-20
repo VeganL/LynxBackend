c29fa6ebb595dcd40db53ebbb6a660b0531d6406
root <root@ip-172-31-92-245.ec2.internal>
2019-05-20 18:01:31 +0000

    20190520 Update to register function

--- backend.py
+++ backend.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 ''' Imports of necessary modules '''
-import cgi,cgitb,re,json
+import cgi,cgitb,re,json,smtplib
 cgitb.enable()
 from mysql.connector import MySQLConnection, Error
 from pythonMySQL_dbConfig import readDbConfig
+from pythonEmailConfig import readEmailConfig
 
 #Defines resulting page/text as json format 
 print('Content-type: application/json\n')
@@ -60,10 +61,26 @@ def dbQ(query,args=None):
 
 #Register function, adds valid information to database
 def register(username,email,password):
-    query = "INSERT INTO accounts(username,email,password) " \
-            "VALUES(%s,%s,%s)"
-    args = (username,email,password)
-    dbIns(query,args)
+    query = "SELECT * FROM accounts WHERE username = '" + str(username) + "'"
+    usernameTaken = dbQ(query)
+    query = "SELECT * FROM accounts WHERE email = '" + str(email) + "'"
+    emailTaken = dbQ(query)
+    if len(usernameTaken) > 0:
+        print('{"err":"Username already taken"}')
+    elif len(emailTaken) > 0:
+        print('{"err":"An account is already registered to this E-mail address"}')
+    else:
+        '''server = smtplib.SMTP('smtp.gmail.com', 587)
+        emailconfig = readEmailConfig()
+        server.login(emailconfig['email'],emailconfig['password'])
+        msg = "[Lynx] E-mail Confirmation\nHey, " + username + ". Please use the following link to verify your email and finish setting up your account: http://lynx.lroy.us/cgi-bin/emailConfirmation?username=" + username + "&email=" + email
+        server.sendmail(emailconfig['email'],email,msg)'''
+
+        query = "INSERT INTO accounts(username,email,password) " \
+                "VALUES(%s,%s,%s)"
+        args = (username,email,password)
+        dbIns(query,args)
+        #print('{"msg":"Please check your E-mail to verify your account"}')
 
 #Login function, returns account_id connected to valid username and password
 def login(username,password):
